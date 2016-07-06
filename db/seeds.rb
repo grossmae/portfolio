5766f2408003fe91cbd4e9796f53728af007df5b
Evan Grossman <grossmae@gmail.com>
2016-07-06 13:31:00 -0700

    Add project info in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,64 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+dougaBox = Project.new(title: "Douga Box",
+                subtitle: "Share files, from your phone to the world",
+                description: "It was all a dream.  I used to read wordup magazine.  Salt and Pepper, Heavy D up in the limousine."
+                )
+dougaBox.icon = Icon.new(image_url: "test.png")
+dougaBox.screenshots << Screenshot.new(image_url: "test.png")
+dougaBox.save
+
+goodFortune = Project.new(title: "Good Fortune",
+                subtitle: "What does the future have in store for you?",
+                description: "It was all a dream.  I used to read wordup magazine.  Salt and Pepper, Heavy D up in the limousine."
+                )
+goodFortune.icon = Icon.new(image_url: "test.png")
+goodFortune.screenshots << Screenshot.new(image_url: "test.png")
+goodFortune.save
+
+musicStream = Project.new(title: "Music Stream",
+                subtitle: "A whole world of music in your pocket",
+                description: "It was all a dream.  I used to read wordup magazine.  Salt and Pepper, Heavy D up in the limousine."
+                )
+musicStream.icon = Icon.new(image_url: "test.png")
+musicStream.screenshots << Screenshot.new(image_url: "test.png")
+musicStream.save
+
+ufoGame = Project.new(title: "UFO Game",
+                subtitle: "The Unidentified Friendly Object",
+                description: "It was all a dream.  I used to read wordup magazine.  Salt and Pepper, Heavy D up in the limousine."
+                )
+ufoGame.icon = Icon.new(image_url: "test.png")
+ufoGame.screenshots << Screenshot.new(image_url: "test.png")
+ufoGame.save
+
+tokaku = Project.new(title: "Tokaku",
+                subtitle: "A whole new way to see ourselves",
+                description: "It was all a dream.  I used to read wordup magazine.  Salt and Pepper, Heavy D up in the limousine."
+                )
+tokaku.icon = Icon.new(image_url: "test.png")
+tokaku.screenshots << Screenshot.new(image_url: "test.png")
+tokaku.save
+
+studyPact = Project.new(title: "StudyPact",
+                subtitle: "Earn while you learn",
+                description: "It was all a dream.  I used to read wordup magazine.  Salt and Pepper, Heavy D up in the limousine."
+                )
+studyPact.icon = Icon.new(image_url: "test.png")
+studyPact.screenshots << Screenshot.new(image_url: "test.png")
+studyPact.save
+
+schooldio = Project.new(title: "Schooldio",
+                subtitle: "Your new academic advisor",
+                description: "It was all a dream.  I used to read wordup magazine.  Salt and Pepper, Heavy D up in the limousine."
+                )
+schooldio.icon = Icon.new(image_url: "test.png")
+schooldio.screenshots << Screenshot.new(image_url: "test.png")
+schooldio.save
+
+coupleCanvas = Project.new(title: "Couple Canvas",
+                subtitle: "Create a work of art and love",
+                description: "It was all a dream.  I used to read wordup magazine.  Salt and Pepper, Heavy D up in the limousine."
+                )
+coupleCanvas.icon = Icon.new(image_url: "test.png")
+coupleCanvas.screenshots << Screenshot.new(image_url: "test.png")
+coupleCanvas.save
+
