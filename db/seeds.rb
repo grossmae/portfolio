9e7ef51a403cfc614dad4a0a23df708d6556e9bb
Evan Grossman <grossmae@gmail.com>
2016-07-29 13:43:11 -0700

    Update schooldio images

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,55 +2,55 @@ dougaBox = Project.new(title: "Douga Box",
                 subtitle: "Share files, from your phone to the world",
                 description: "It was all a dream.  I used to read wordup magazine.  Salt and Pepper, Heavy D up in the limousine."
                 )
-dougaBox.icon = Icon.new(image_url: "db-icon.png")
-dougaBox.screenshots << Screenshot.new(image_url: "db-shot1.png")
-dougaBox.screenshots << Screenshot.new(image_url: "db-shot2.png")
-dougaBox.screenshots << Screenshot.new(image_url: "db-shot3.png")
+dougaBox.icon = Icon.new(image_url: "dougabox/db-icon.png")
+dougaBox.screenshots << Screenshot.new(image_url: "dougabox/db-shot1.png")
+dougaBox.screenshots << Screenshot.new(image_url: "dougabox/db-shot2.png")
+dougaBox.screenshots << Screenshot.new(image_url: "dougabox/db-shot3.png")
 dougaBox.save
 
 goodFortune = Project.new(title: "Good Fortune",
                 subtitle: "What does the future have in store for you?",
                 description: "It was all a dream.  I used to read wordup magazine.  Salt and Pepper, Heavy D up in the limousine."
                 )
-goodFortune.icon = Icon.new(image_url: "gf-icon.png")
-goodFortune.screenshots << Screenshot.new(image_url: "gf-shot1.png")
-goodFortune.screenshots << Screenshot.new(image_url: "gf-shot2.png")
-goodFortune.screenshots << Screenshot.new(image_url: "gf-shot3.png")
+goodFortune.icon = Icon.new(image_url: "good-fortune/gf-icon.png")
+goodFortune.screenshots << Screenshot.new(image_url: "good-fortune/gf-shot1.png")
+goodFortune.screenshots << Screenshot.new(image_url: "good-fortune/gf-shot2.png")
+goodFortune.screenshots << Screenshot.new(image_url: "good-fortune/gf-shot3.png")
 goodFortune.save
 
 musicStream = Project.new(title: "Music Stream",
                 subtitle: "A whole world of music in your pocket",
                 description: "It was all a dream.  I used to read wordup magazine.  Salt and Pepper, Heavy D up in the limousine."
                 )
-musicStream.icon = Icon.new(image_url: "ms-icon.png")
-musicStream.screenshots << Screenshot.new(image_url: "ms-shot1.png")
-musicStream.screenshots << Screenshot.new(image_url: "ms-shot2.png")
-musicStream.screenshots << Screenshot.new(image_url: "ms-shot3.png")
+musicStream.icon = Icon.new(image_url: "music-stream/ms-icon.png")
+musicStream.screenshots << Screenshot.new(image_url: "music-stream/ms-shot1.png")
+musicStream.screenshots << Screenshot.new(image_url: "music-stream/ms-shot2.png")
+musicStream.screenshots << Screenshot.new(image_url: "music-stream/ms-shot3.png")
 musicStream.save
 
 ufoGame = Project.new(title: "UFO Game",
                 subtitle: "The Unidentified Friendly Object",
                 description: "It was all a dream.  I used to read wordup magazine.  Salt and Pepper, Heavy D up in the limousine."
                 )
-ufoGame.icon = Icon.new(image_url: "ufo-icon.png")
-ufoGame.screenshots << Screenshot.new(image_url: "ufo-shot1.png")
+ufoGame.icon = Icon.new(image_url: "ufo-game/ufo-icon.png")
+ufoGame.screenshots << Screenshot.new(image_url: "ufo-game/ufo-shot1.png")
 ufoGame.save
 
 cameraDeBelle = Project.new(title: "Camera de Belle",
                 subtitle: "A whole new way to see ourselves",
                 description: "It was all a dream.  I used to read wordup magazine.  Salt and Pepper, Heavy D up in the limousine."
                 )
-cameraDeBelle.icon = Icon.new(image_url: "cdb-icon.png")
-cameraDeBelle.screenshots << Screenshot.new(image_url: "cdb-shot1.png")
-cameraDeBelle.screenshots << Screenshot.new(image_url: "cdb-shot1.png")
-cameraDeBelle.screenshots << Screenshot.new(image_url: "cdb-shot1.png")
+cameraDeBelle.icon = Icon.new(image_url: "camera-de-belle/cdb-icon.png")
+cameraDeBelle.screenshots << Screenshot.new(image_url: "camera-de-belle/cdb-shot1.png")
+cameraDeBelle.screenshots << Screenshot.new(image_url: "camera-de-belle/cdb-shot1.png")
+cameraDeBelle.screenshots << Screenshot.new(image_url: "camera-de-belle/cdb-shot1.png")
 cameraDeBelle.save
 
 studyPact = Project.new(title: "StudyPact",
                 subtitle: "Earn while you learn",
                 description: "It was all a dream.  I used to read wordup magazine.  Salt and Pepper, Heavy D up in the limousine."
                 )
-studyPact.icon = Icon.new(image_url: "test.png")
+studyPact.icon = Icon.new(image_url: "study-pact/sp-icon.png")
 studyPact.screenshots << Screenshot.new(image_url: "test.png")
 studyPact.save
 
@@ -58,19 +58,19 @@ schooldio = Project.new(title: "Schooldio",
                 subtitle: "Your new academic advisor",
                 description: "It was all a dream.  I used to read wordup magazine.  Salt and Pepper, Heavy D up in the limousine."
                 )
-schooldio.icon = Icon.new(image_url: "schooldio-icon.png")
-schooldio.screenshots << Screenshot.new(image_url: "schooldio-shot1.png")
-schooldio.screenshots << Screenshot.new(image_url: "schooldio-shot2.png")
-schooldio.screenshots << Screenshot.new(image_url: "schooldio-shot3.png")
+schooldio.icon = Icon.new(image_url: "schooldio/schooldio-icon.png")
+schooldio.screenshots << Screenshot.new(image_url: "schooldio/schooldio-shot1.png")
+schooldio.screenshots << Screenshot.new(image_url: "schooldio/schooldio-shot2.png")
+schooldio.screenshots << Screenshot.new(image_url: "schooldio/schooldio-shot3.png")
 schooldio.save
 
 coupleCanvas = Project.new(title: "Couple Canvas",
                 subtitle: "Create a work of art and love",
                 description: "It was all a dream.  I used to read wordup magazine.  Salt and Pepper, Heavy D up in the limousine."
                 )
-coupleCanvas.icon = Icon.new(image_url: "cc-icon.png")
-coupleCanvas.screenshots << Screenshot.new(image_url: "cc-shot1.png")
-coupleCanvas.screenshots << Screenshot.new(image_url: "cc-shot2.png")
-coupleCanvas.screenshots << Screenshot.new(image_url: "cc-shot3.png")
+coupleCanvas.icon = Icon.new(image_url: "couple-canvas/cc-icon.png")
+coupleCanvas.screenshots << Screenshot.new(image_url: "couple-canvas/cc-shot1.png")
+coupleCanvas.screenshots << Screenshot.new(image_url: "couple-canvas/cc-shot2.png")
+coupleCanvas.screenshots << Screenshot.new(image_url: "couple-canvas/cc-shot3.png")
 coupleCanvas.save
 
