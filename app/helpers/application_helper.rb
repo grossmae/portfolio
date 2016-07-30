d8fc7e3591f56aa84c30e4a01c90dd9f12611892
Evan Grossman <grossmae@gmail.com>
2016-07-29 21:14:31 -0700

    Setup image selection on project show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,8 @@ module ApplicationHelper
       page_title
     end
   end
+
+  def link_to_image(image_path, target_link, options={})
+    link_to(image_tag(image_path, :border => "0"), target_link, options)
+  end
 end
