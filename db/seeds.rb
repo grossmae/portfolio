b9d8f7d7f1ea481aa6d7a02193195d1c7a5638d6
Evan Grossman <grossmae@gmail.com>
2016-08-01 15:26:50 -0700

    Fix images and content in the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,16 @@
 dougaBox = Project.new(title: "Douga Box",
                 subtitle: "Share files, from your phone to the world",
-                description: "It was all a dream.  I used to read wordup magazine.  Salt and Pepper, Heavy D up in the limousine."
+                description: "File sharing app with a simple interface.  Supports transfers of files over 1GB.  Features a folder and file organization system."
                 )
 dougaBox.icon = Icon.new(image_url: "dougabox/db-icon.png")
-dougaBox.screenshots << Screenshot.new(image_url: "dougabox/db-shot1.png")
-dougaBox.screenshots << Screenshot.new(image_url: "dougabox/db-shot2.png")
+dougaBox.screenshots << Screenshot.new(image_url: "dougabox/db-shot1.jpg")
+dougaBox.screenshots << Screenshot.new(image_url: "dougabox/db-shot2.jpg")
 dougaBox.screenshots << Screenshot.new(image_url: "dougabox/db-shot3.png")
 dougaBox.save
 
 goodFortune = Project.new(title: "Good Fortune",
                 subtitle: "What does the future have in store for you?",
-                description: "It was all a dream.  I used to read wordup magazine.  Salt and Pepper, Heavy D up in the limousine."
+                description: "Gives users advice on life, career, and love based on their birthday and personality.  Features subscription based in app purchase.  Prophetic accuracy not tested. "
                 )
 goodFortune.icon = Icon.new(image_url: "good-fortune/gf-icon.png")
 goodFortune.screenshots << Screenshot.new(image_url: "good-fortune/gf-shot1.png")
@@ -20,7 +20,7 @@ goodFortune.save
 
 musicStream = Project.new(title: "Music Stream",
                 subtitle: "A whole world of music in your pocket",
-                description: "It was all a dream.  I used to read wordup magazine.  Salt and Pepper, Heavy D up in the limousine."
+                description: "Search for music files available on the internet.  Supports offline song caching, album art, and playlist organization.  Reached the top of the free apps category in Japanese iTunes store a few times."
                 )
 musicStream.icon = Icon.new(image_url: "music-stream/ms-icon.png")
 musicStream.screenshots << Screenshot.new(image_url: "music-stream/ms-shot1.png")
@@ -30,7 +30,7 @@ musicStream.save
 
 ufoGame = Project.new(title: "UFO Game",
                 subtitle: "The Unidentified Friendly Object",
-                description: "It was all a dream.  I used to read wordup magazine.  Salt and Pepper, Heavy D up in the limousine."
+                description: "Small side project to play around with 3D games in SceneKit.  I want to make a game featuring a lonely UFO traveling between planets to change their ecology.  Currently stuck in game design, if you have any ideas."
                 )
 ufoGame.icon = Icon.new(image_url: "ufo-game/ufo-icon.png")
 ufoGame.screenshots << Screenshot.new(image_url: "ufo-game/ufo-shot1.png")
@@ -38,17 +38,17 @@ ufoGame.save
 
 cameraDeBelle = Project.new(title: "Camera de Belle",
                 subtitle: "A whole new way to see ourselves",
-                description: "It was all a dream.  I used to read wordup magazine.  Salt and Pepper, Heavy D up in the limousine."
+                description: "Using image mirroring, the app show users their facial symetry.  An in app blog provided makeup and other tips to improve symetry.  A calendar system allowed users to save images for each day and record progress."
                 )
 cameraDeBelle.icon = Icon.new(image_url: "camera-de-belle/cdb-icon.png")
 cameraDeBelle.screenshots << Screenshot.new(image_url: "camera-de-belle/cdb-shot1.png")
-cameraDeBelle.screenshots << Screenshot.new(image_url: "camera-de-belle/cdb-shot1.png")
-cameraDeBelle.screenshots << Screenshot.new(image_url: "camera-de-belle/cdb-shot1.png")
+cameraDeBelle.screenshots << Screenshot.new(image_url: "camera-de-belle/cdb-shot2.png")
+cameraDeBelle.screenshots << Screenshot.new(image_url: "camera-de-belle/cdb-shot3.png")
 cameraDeBelle.save
 
 studyPact = Project.new(title: "StudyPact",
                 subtitle: "Earn while you learn",
-                description: "It was all a dream.  I used to read wordup magazine.  Salt and Pepper, Heavy D up in the limousine."
+                description: "Users could commit to studying a certain ammount of hours each week.  The Android app would track their activity in educational apps.  At the end of the week, users who failed to reach their goal would have to pay and their money would be distributed to all the successful users."
                 )
 studyPact.icon = Icon.new(image_url: "study-pact/sp-icon.png")
 studyPact.screenshots << Screenshot.new(image_url: "test.png")
@@ -56,7 +56,7 @@ studyPact.save
 
 schooldio = Project.new(title: "Schooldio",
                 subtitle: "Your new academic advisor",
-                description: "It was all a dream.  I used to read wordup magazine.  Salt and Pepper, Heavy D up in the limousine."
+                description: "This app provided high school students in Japan information about colleges they were interested in from real student perspectives.  College students would show a side of the college unaccessible to most high school students who lived out of the area.  Videos and blogs would provide tips on entrance exams, classes, and student life."
                 )
 schooldio.icon = Icon.new(image_url: "schooldio/schooldio-icon.png")
 schooldio.screenshots << Screenshot.new(image_url: "schooldio/schooldio-shot1.png")
@@ -66,7 +66,7 @@ schooldio.save
 
 coupleCanvas = Project.new(title: "Couple Canvas",
                 subtitle: "Create a work of art and love",
-                description: "It was all a dream.  I used to read wordup magazine.  Salt and Pepper, Heavy D up in the limousine."
+                description: "The app provided an exclusive connection to one other phone.  Users would share a canvas to paint on and see eachother's brush strokes.  Over time, their art would fade into the background.  This was created for a short hackathon in Osaka."
                 )
 coupleCanvas.icon = Icon.new(image_url: "couple-canvas/cc-icon.png")
 coupleCanvas.screenshots << Screenshot.new(image_url: "couple-canvas/cc-shot1.png")
